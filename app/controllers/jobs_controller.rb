2ffc644a6365f6d714607b7ca569a106ccf3745d
wangxing <18912540921@163.com>
2018-03-01 19:31:37 +0800

    order by wage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,14 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create edit update destroy]
 
   def index
-    @jobs = Job.all.where(is_hidden: false).order('created_at DESC')
+    @jobs = case params[:order]
+            when 'by_upper_bound'
+              Job.all.published.order('wage_upper_bound DESC')
+            when 'by_lower_bound'
+              Job.all.published.order('wage_lower_bound DESC')
+            else
+              Job.all.published.recent
+            end
   end
 
   def show
